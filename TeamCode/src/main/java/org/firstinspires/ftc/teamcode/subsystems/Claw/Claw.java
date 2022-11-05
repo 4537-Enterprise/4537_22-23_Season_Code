fecb9ea667c2286ca77cd92c66cd3184049946bb
JohnSchmenk <schmenk.5@daytonstemschool.org>
2022-11-05 09:57:27 -0400

    WIP - claw class stuf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw/Claw.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.subsystems.Claw;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Claw {
+    // identify servo port zero
+    private Servo ClawServo;
+}
